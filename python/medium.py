feaeef21024583b2268c16725e985db43afd8e27
delozath <delozath@gmail.com>
2022-12-08 18:01:11 -0600

    Ni idea porqué se descargó

--- python/medium.py
+++ python/medium.py
@@ -6,30 +6,35 @@ import re
 import pdb
 
 class Remove_All_Adjacent_Duplicates_in_String_II():
+    
+    
     def removeDuplicates(self, s: str, k: int) -> str:
-        lst  = lambda s, k: [s[i:i+k] for i in range(0,len(s)-k+1)]
         expr = re.compile(r'(.)\1{%d,}'%(k-1))
-        
-        ss = lst(s, k)
-        rm = []
-        for n, l in enumerate(ss):
-            eval = expr.search(l)
-            if eval:
-                print(l)
-                for i in range(k):
-                    rm.append(n+i)
-        
-        result = []
-        for i in range(len(ss)):
-            if not (i in rm):
-                result.append(ss[i])
-        
-        print(result)
+        #
+        i   = 0
+        r   = ''
+        end = 1
+        pdb.set_trace()
+        while end:
+            end = 0
+            i   = 0
+            r   = ''
+            while i < len(s):
+                ss   = s[i:i+k]
+                eval = expr.search(ss)
+                if not eval:
+                    r += ss[0]
+                    i += 1
+                else:
+                    i   += k
+                    end += 1
+            s = r
+            print(s)
         pdb.set_trace()
 
 def main():
-    s = 'lakkksklsllkjjlsjdsl'
-    k = 2
+    s = 'ppkkkknopsstapttt'
+    k = 3
     Remove_All_Adjacent_Duplicates_in_String_II().removeDuplicates(s, k)
 
 if __name__ == '__main__':
